51073b28aad30095a770372ad47682e784a96e3f
Gabri432 <gabrielegatti432@gmail.com>
2022-09-27 20:28:32 +0200

    Implementing package testing.

---
+++ internal_functions_test.go
@@ -0,0 +1,21 @@
+package equationsolver
+
+import (
+	"testing"
+)
+
+var testPolynom = "x^3+2x^2+x+1=0"
+
+func TestIsSign(t *testing.T) {
+	mapOfBools := map[bool]int{}
+	mapOfBools[isSign("+")]++
+	mapOfBools[isSign("-")]++
+	mapOfBools[isSign("=")]++
+	mapOfBools[isSign("/")]++
+	if mapOfBools[false] != 1 {
+		t.Fatalf("Expected determinant should be one, got %d.", mapOfBools[false])
+	}
+	if mapOfBools[true] != 3 {
+		t.Fatalf("Expected determinant should be one, got %d.", mapOfBools[true])
+	}
+}
